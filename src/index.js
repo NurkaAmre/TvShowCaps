b3282e6aee3802946b746bf2136ab196b601f0d7
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-10-27 15:44:57 +0600

    Add functionality to like button and fetched likes data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 
+
 const API = 'https://api.tvmaze.com/shows/1/episodes';
 const likesAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/OpXkQhwbfD4wnLSWy6wV/likes';
 
@@ -7,96 +8,115 @@ const displayEpisode = document.querySelector('.episode');
 const previous = document.querySelector('.previous');
 const next = document.querySelector('.next');
 const pageNum = document.querySelector('.page-numbers');
+const body = document.querySelector('body');
 
-let showArray = [];
+let showsArray = [];
 
 const getData = async (API) => {
   const result = await fetch(API);
   return result;
 };
 
-function displayCards(episode) {
+const likeShow = async (id, likesNumber, likesBtn) => {
+  await fetch(likesAPI, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: id,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then(() => {
+    likesNumber.innerHTML = parseInt(likesNumber.innerHTML, 10) + 1;
+    const i = likesBtn.querySelector('i');
+    i.classList.remove('fa-regular');
+    i.classList.add('fa-solid');
+  });
+};
+
+
+function createShowCard(episode) {
   const div = document.createElement('div');
   div.classList.add('card');
   div.innerHTML = `
   <h2 class="card-title">${episode.name}</h2>
-             <img class="card-img-top img" src=${episode.image.medium} alt="Card image cap">
-             <div class="card-body">       
-                <p class="card-text">${episode.summary}</p>
-                <button type="button" class="btn btn-link btn-comment">Comments</i></button>
-                <div class="like">
-                <button type="button" onclick="count()"
-                 class="btn btn-link btn-like"><i class="fa-solid fa-heart"></i></button>
-                <span class="likeCount">0</span>
-                </div>
-                </div>`;
-
+  <img class="card-img-top img" src=${episode.image.medium} alt="">
+  <div class="card-body">       
+     <p class="card-text">${episode.summary}</p>
+     <button type="button" class="btn btn-link btn-comment">Comments</i></button>
+     <div class="likes">
+     <button type="button"
+      class="btn btn-link like-count"><i class="fa-regular fa-heart like-icon"></i></button>
+     <span class="likeCount">0</span>
+     </div>
+     </div>
+  `;
   const likesNumber = div.querySelector('.likeCount');
-  if ('like' in episode) {
-    likesNumber.innerHTML = episode.like;
+  if ('likes' in episode) {
+    likesNumber.innerHTML = episode.likes;
   }
+
+  const likeBtn = div.querySelector('.like-count');
+  likeBtn.addEventListener('click', () => {
+    likeShow(episode.id, likesNumber, likeBtn);
+  });
+
+
   return div;
 }
 
 const displayShows = (shows, pageNumber) => {
-  shows.slice(pageNumber * 10 - 10, 9).forEach((show) => {
-    const div = displayCards(show);
+  shows.slice(pageNumber * 10 - 10, pageNumber * 9).forEach((show) => {
+    const div = createShowCard(show);
     displayEpisode.append(div);
   });
 };
 
 function loadNext(pageNumber, shows) {
   const nextPage = pageNumber + 1;
-  if (pageNumber + 1 < 6) {
+  if (nextPage < 5 ) {
     pageNum.innerHTML = nextPage;
     displayEpisode.innerHTML = '';
+    getData(API)
+      .then((response) => response.json())
+      .then((json) => displayShows(json, pageNumber + 1));
     displayShows(shows, nextPage);
   }
 }
 
 function loadPrevious(pageNumber, shows) {
   const previousPage = pageNumber - 1;
-  if (pageNumber > 0) {
+  if (previousPage > 0) {
     pageNum.innerHTML = previous;
     displayEpisode.innerHTML = '';
+    getData(API)
+      .then((response) => response.json())
+      .then((json) => displayShows(json, pageNumber - 1));
     displayShows(shows, previousPage);
   }
 }
 
 previous.addEventListener('click', () => {
-  loadPrevious(parseInt(pageNum.innerHTML, 10), showArray);
+  loadPrevious(parseInt(pageNum.innerHTML, 10), showsArray);
 });
 
 next.addEventListener('click', () => {
-  loadNext(parseInt(pageNum.innerHTML, 10), showArray);
+  loadNext(parseInt(pageNum.innerHTML, 10), showsArray);
 });
 
 getData(API)
+  .then((response) => response.json())
+  .then((json) => displayShows(json, 1));
+getData(API)
   .then((response1) => response1.json())
   .then((shows) => {
-    showArray = shows;
+    showsArray = shows;
     getData(likesAPI)
-      .then((response2) => response2.json())
-      .then((like) => {
-        like.forEach((item, i) => {
-          showArray[i].like = item.like;
+      .then((response) => response.json())
+      .then((likes) => {
+        likes.forEach((item, i) => {
+          showsArray[i].likes = item.likes;
         });
-        displayShows(showArray, 1);
+        displayShows(showsArray, 1);
       });
   });
-
-//   const createApp = async () => {
-//     const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/', {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         name: 'My App',
-//       }),
-//       headers: {
-//         'Content-type': 'application/json; charset=UTF-8',
-//       },
-//     });
-//     const data = await response.text();
-//     console.log(data);
-//  };
-
-// //  createApp()
